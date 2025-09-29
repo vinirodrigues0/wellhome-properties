cc49d28d2d4f6a450fb8bf0eecd0b21fc5ebe350
Vinicius Rodrigues <vini77589@gmail.com>
2025-09-29 16:16:43 -0300

    Configurando projeto para deploy no Render

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const propertyRoutes = require('./routes/propertyRoutes');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(cors());
